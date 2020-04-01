08bb53834b78e79409d226257d0222d1414aa4a5
unknown <mohmud141@gmail.com>
2020-04-01 20:38:28 +0200

    Learned Console Alert, Documetn.write, how to run js

---
+++ js.js
@@ -0,0 +1,18 @@
+
+
+
+// Learned Console Alert, Documetn.write, how to run js 
+// console.log('myName is mohamud')
+
+console.log('11 == 10');
+console.log("I am " + (30+9) + " years old");
+console.log("i am going to marry "   +  2 +  " wives");
+
+// I am going to marry 2 wives
+// I have 2 carss and 2 computers
+console.log('I have '+ 2 +' cars and ' + 2 + ' computers');
+console.log('I have ', 2 ,' cars and ' , 2 , ' computers');
+
+console.log("Hellow", 44)
+console.log("Hellow " ,44)
+alert('hello world')
