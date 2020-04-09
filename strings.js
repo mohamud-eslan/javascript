2aa0a34652c64dac5f32de0dae269301148c8a52
unknown <mohmud141@gmail.com>
2020-04-09 20:46:51 +0200

    added string manipulation

---
+++ strings.js
@@ -0,0 +1,70 @@
+// add to string
+// lenght of string
+// lowercase , uppercase
+// concat string to number
+// replace every 'a' in 'banana' with 'u'
+        // console.log(fruit.replace(/a/g, "u"));
+        // mystring.replace("removethis", "insertThis")
+
+// check if 'name' is in 'hi, my name is mark'
+        // let mystr = 'hi, my name is mark';
+
+// locate index of 'welcome' in  'hello, welcome to my world'
+// trim trailing and proceeding spaces in "  hello ", should be "hello"
+// convert string into list 
+// split by charactor e.g ','
+// substring/slice the words 'hello' in  'hi, hello , wellocme'
+// get the charAt at position 3 in 'hello'
+// compare two strings if they are the same or diff lengths
+
+
+
+
+// let fruit ='hello, wecome to my world'
+// console.log(fruit.indexOf('wecome'));
+
+// let password = "         1998                "
+// // console.log(password.trim());
+// let word = 'hel lo' // ['h', 'e', 'l', 'l', 'e']
+// // console.log(word.split(" "));
+
+// var str = "How are you doing today?";
+
+// //var res = str.split(" ", 3);
+// // console.log(str.slice(8, 17));
+
+// console.log(str[4]);
+// console.log(str.charAt(4));
+
+// let aa = "aaa";
+// let bb = "bb"
+// // check if aa and bb have the same length
+// if (aa.length===bb.length) {
+//     console.log('same')
+// }else{
+//     console.log('not same')
+    
+// }
+
+// let animals = 'camel';
+// let somanimal = ' cow';
+// let totalanimal = animals + somanimal
+// console.log(totalanimal);
+// console.log(totalanimal.split())
+
+
+// let secret = ' password ';
+//  console.log(secret.trim(' '));
+
+// let jjj = 'kiwi'
+// let lll = 'strowberry'
+// if (jjj === lll) {
+//             console.log('same')
+// } else {
+//     console.log('not')
+    
+// }
+
+let myWay = 'one aaaaaaway';
+console.log(myWay.replace(/a/g, 'u'));
+
